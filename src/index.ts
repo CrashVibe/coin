aa5c0fab1be7c3a30c121cc512aebaab4f01ba1c
MrlingXD <wlingzhenyu@163.com>
2025-07-20 07:31:02 +0800

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,101 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Service } from 'koishi';
+import { applyModel } from './model';
 
-export const name = 'coin'
+export const name = 'coin';
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export async function apply(ctx: Context) {
+    ctx.command('次元币', '查看你的次元币余额').action(async ({ session }) => {
+        if (!session || !session.userId) {
+            console.warn('次元币命令需要用户上下文，无法获取用户 ID。');
+            return '出现一点错误，请稍后再试';
+        }
+        const coinnum = await ctx.coin.getCoin(session.userId);
+        return `你当前的次元币余额为：${coinnum}`;
+    });
+}
+
+export default class Coin extends Service {
+    static inject = ['database'];
+
+    constructor(ctx: Context) {
+        applyModel(ctx);
+        super(ctx, 'coin');
+    }
+
+    /**
+     * 获取用户的金币数量
+     *
+     * @param user 用户 ID
+     * @returns 用户的金币数量
+     */
+    async getCoin(user: string): Promise<number> {
+        let result = await this.ctx.database.get('coin', user);
+        if (result.length === 0) {
+            await this.ctx.database.set('coin', user, { coin: 0 });
+            return 0;
+        }
+        return result[0].coin || 0;
+    }
+
+    /**
+     * 设置用户的金币数量
+     *
+     * @param user 用户 ID
+     * @param coin 金币数量
+     */
+    async setCoin(user: string, coin: number, source: string): Promise<void> {
+        await this.ctx.database.set('coin', user, { coin });
+        await this.ctx.database.create('coin_source_record', {
+            user,
+            coin,
+            date: new Date(),
+            source
+        });
+    }
+
+    /**
+     * 加减用户的金币数量
+     *
+     * @param user 用户 ID
+     * @param coin 金币数量，可以为正数或负数
+     * @returns 是否成功调整金币数量
+     */
+    async adjustCoin(user: string, coin: number, source: string): Promise<boolean> {
+        const data = await this.ctx.database.get('coin', user);
+        const currentCoin = data[0]?.coin || 0;
+        if (!this.hasEnoughCoin(user, coin)) {
+            return false;
+        }
+        await this.ctx.database.upsert('coin', (row) => [{ user: user, coin: currentCoin + coin }]);
+        await this.ctx.database.create('coin_source_record', {
+            user,
+            coin,
+            source,
+            date: new Date()
+        });
+        return true;
+    }
+
+    /**
+     * 检查用户是否有足够的金币
+     *
+     * @param user 用户 ID
+     * @param coin 要求的金币数量
+     * @returns 是否有足够的金币
+     */
+    async hasEnoughCoin(user: string, coin: number): Promise<boolean> {
+        const data = await this.ctx.database.get('coin', user);
+        const currentCoin = data[0]?.coin || 0;
+        return currentCoin >= coin;
+    }
+}
+
+declare module 'koishi' {
+    interface Context {
+        coin: Coin;
+    }
 }
---
+++ src/model.ts
@@ -0,0 +1,49 @@
+import { Context } from 'koishi';
+import Coin from '.';
+
+declare module 'koishi' {
+    interface Tables {
+        coin: CoinModel;
+        coin_source_record: CoinSourceRecord;
+    }
+}
+
+export interface CoinModel {
+    user: string;
+    coin: number;
+}
+
+export interface CoinSourceRecord {
+    id: number;
+    user: CoinModel['user'];
+    coin: number;
+    source: string;
+    date: Date;
+}
+
+export function applyModel(ctx: Context) {
+    ctx.model.extend(
+        'coin_source_record',
+        {
+            id: { type: 'unsigned' },
+            user: { type: 'string' },
+            coin: { type: 'integer' },
+            source: { type: 'string' },
+            date: { type: 'date' }
+        },
+        {
+            primary: 'id',
+            autoInc: true
+        }
+    );
+    ctx.model.extend(
+        'coin',
+        {
+            user: { type: 'string' },
+            coin: { type: 'integer', initial: 0 }
+        },
+        {
+            primary: 'user'
+        }
+    );
+}
